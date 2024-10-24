48f511087888a25203a3a3fbe077acfd487d0523
Amy Adams <amy.adams.f9@gmail.com>
2024-10-24 15:34:57 +0100

    Initial commit

---
+++ strings test.py
@@ -0,0 +1,12 @@
+my_string = "Hello world! Here is my first program!"
+# print(my_string)
+# print(my_string[9])
+# print(my_string[0:6])
+# print(my_string[-1])
+
+my_name = input("Please enter your name here: ")
+print("Your name is: " + my_name)
+my_age = input("Please do not enter your age here: ")
+print('Your age is: ' + my_age)
+
+
